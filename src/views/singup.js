23ae819ec454b8cf69073706c9182aef6799f3b4
Age-Force <56095479+Age-Force@users.noreply.github.com>
2019-11-08 05:48:19 +0100

    This is the first commit for the Vue website project
    Progress on vue

---
+++ src/views/singup.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+
+Vue.use(signupApp)
+
+var signupApp = new Vue({
+    el: '#signup',
+    data: {
+        username: '',
+        email: '',
+        password: '',
+    },
+    methods: {
+        onSubmit: function () {
+            // this is to check if the email already exists
+            var users = '';
+            var newUsername = this.username;
+            var newName = this.name;
+            var newEmail = this.email;
+            if (localStorage.getItem('users')) { // 'users' is an array of objects
+                users = JSON.parse(localStorage.getItem('users'));
+            }
+            if (users) {
+                if (users.some(function (user) {
+                    return user.email === newEmail
+                })) {
+                    alert('Email or username already exists!');
+                    return;
+                }
+                users.push({'username':newUsername, 'name':newName,'email': newEmail, 'password': this.password});
+                localStorage.setItem('users', JSON.stringify(users));
+                window.location.replace ("Subject.html");
+                alert("welcome to AFTER SCHOOL EXTRAS ")
+                return false;
+
+            }
+            else {
+                users = [{'username':newUsername, 'name':newName,'email': newEmail, 'password': this.password}];
+                localStorage.setItem('users', JSON.stringify(users));
+            }
+        }
+    }
+})
